1d47cbf9082d929bc5b9160a553ec215ea3f62de
Angelo Pinto <angelopinto.gs@gmail.com>
2025-04-30 18:44:38 +0200

    add general styling layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="">
-      hELLO
+    <div id="root" className="flex justify-center items-center h-full">
+      <header className="fixed top-0 w-full border-b border-b-[#ccc]">
+        <div className="w-full p-4">QUOTE GENERATOR</div>
+      </header>
+      <div className="flex flex-col gap-4">
+        <div className="w-full">
+          <Image
+            src="/assets/placeholder-image.webp"
+            alt="placeholder"
+            width="350"
+            height="350"
+          />
+        </div>
+        <div>
+          <div>Behold! Cat wisdom</div>
+          <div className="w-[50ch]">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
