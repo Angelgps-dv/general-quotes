620d67c0d323edebb35601a0044c5518ddefd9f2
Angelo Pinto <angelopinto.gs@gmail.com>
2025-05-01 18:09:54 +0200

    reworked layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,27 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div id="root" className="flex justify-center items-center h-full">
-      <header className="w-full fixed top-0 w-full shadow-[inset_0_-7px_6px_-6px_#23272fcc]">
-        <div className="w-full p-4">QUOTE GENERATOR</div>
+      <header className="w-full fixed top-0 z-5000 w-full shadow-[2px_5px_3px_rgba(0,0,0,0.2)]">
+        <div className="w-full p-4 bg-[#21242a]">QUOTE GENERATOR</div>
       </header>
-      <div className="flex flex-col gap-4 mx-[4rem] my-0 md:w-1/2 md:mx-auto">
-        <div>AUTHOR: John Author</div>
-        <div className="overflow-hidden rounded border-2 border-[#46454c] max-w-[630px] min-w-[240px]">
+      <div className="max-w-4xl mx-auto border-2 border-gray-700 p-6 bg-black text-white rounded-md font-serif">
+        <div className="text-center text-xl mb-4">Thomas Jefferson</div>
+        <div className="flex flex-col md:flex-row items-center gap-6">
           <Image
+            width={250}
+            height={250}
             src="/assets/placeholder-image.webp"
             alt="placeholder"
-            width="350"
-            height="350"
-            className="w-full"
+            className="w-55 h-55 object-cover rounded-md"
           />
-        </div>
-        <div className="">
-          <div className="leading-[28px]">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
+          <p className="text-lg leading-relaxed w-[40ch]">
+            “Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry standard dummy text ever
             since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book.
-          </div>
+            scrambled it to make a type specimen book.”
+          </p>
         </div>
+        <div className="mt-6 text-center text-sm text-gray-400">1 / 10</div>
       </div>
     </div>
   );
